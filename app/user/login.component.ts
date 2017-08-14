1c4795fb6c5e1603b35393858d7102077308401b
Sathishkumar <be.sathish@gmail.com>
2017-08-14 20:27:19 +0530

    Multiple Validation for Reactive forms

--- app/user/login.component.ts
+++ app/user/login.component.ts
@@ -1,27 +1,25 @@
 import { Component } from '@angular/core'
-import { AuthService} from './auth.service'
-import { Router} from '@angular/router'
+import { AuthService } from './auth.service'
+import { Router } from '@angular/router'
 
 @Component({
   templateUrl: 'app/user/login.component.html',
-  styles: [
-    `
+  styles: [ `
       em {float: right; color: #E05C65; padding-left: 10px}
-    `
-  ]
+    `]
 })
 export class LoginComponent {
-     
-constructor(private authService: AuthService,private router: Router){
 
-}
+  constructor(private authService: AuthService, private router: Router) {
 
-  login(formValues){
-    this.authService.loginUser(formValues.userName,formValues.password)
+  }
+
+  login(formValues) {
+    this.authService.loginUser(formValues.userName, formValues.password)
     this.router.navigate(['events'])
   }
 
-  cancel(){
+  cancel() {
     this.router.navigate(['events'])
   }
 }
--- app/user/profile.component.ts
+++ app/user/profile.component.ts
@@ -5,25 +5,43 @@ import { Router } from '@angular/router'
 
 @Component({
   templateUrl: 'app/user/profile.component.html',
+  styles: [`
+      em {float: right; color: #E05C65; padding-left: 10px;}
+      .error input { background-color:#E3C3C5;}
+      .error ::-webkit-input-placeholder {color: #999;}
+      .error ::-moz-placeholder {color: #999;} 
+      .error :-moz-placeholder {color: #999;}
+      .error ::ms-input-placeholder {color: #999;}
+
+    `]
 })
 export class ProfileComponent implements OnInit {
   profileForm: FormGroup
-
+  private firstName: FormControl
+  private lastName: FormControl
   constructor(private authService: AuthService, private router: Router) {
 
   }
 
   ngOnInit(): void {
-    let firstName = new FormControl(this.authService.currentUser.firstName, Validators.required)
-    let lastName = new FormControl(this.authService.currentUser.lastName, Validators.required)
+    this.firstName = new FormControl(this.authService.currentUser.firstName, 
+      [Validators.required, Validators.pattern('[a-zA-Z].*')])
+    this.lastName = new FormControl(this.authService.currentUser.lastName, Validators.required)
     this.profileForm = new FormGroup({
-      firstName: firstName,
-      lastName: lastName
+      firstName: this.firstName,
+      lastName: this.lastName
     })
   }
 
+  validateFirstName(){
+    return this.firstName.valid || this.firstName.untouched
+  }
+
+  validateLastName(){
+    return this.lastName.valid || this.lastName.untouched
+  }
   saveProfile(formValues) {
-    if(this.profileForm.valid){
+    if (this.profileForm.valid) {
       this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
       this.router.navigate(['events'])
     }
