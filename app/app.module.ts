47afa7218fe8f770eebeb88869711dd4d3777125
Sathishkumar <be.sathish@gmail.com>
2017-08-14 17:05:44 +0530

    Organize imports using index

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { RouterModule } from "@angular/router"
+import {
+    EventsListComponent,
+    EventThumbnailComponent,
+    EventService,
+    EventDetailsComponent,
+    CreateEventComponent,
+    EventRouteActivator,
+    EventListResolver
+} from './events/index'
+
 import { EventsAppComponent } from './events-app.component'
 import { NavBarComponent } from './nav/navbar.component'
-import { EventsListComponent } from './events/events-list.component'
-import { EventThumbnailComponent } from './events/event-thumbnail.component'
-import { EventService } from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service'
-import { EventDetailsComponent } from './events/event-details/event-details.component'
 import { appRoutes } from './routes'
-import { CreateEventComponent } from './events/create-event.component'
 import { Error404Component } from './errors/404.component'
-import { EventRouteActivator } from './events/event-details/event-router-activator.service'
-import { EventListResolver} from './events/events-list-resolver.service'
 
 @NgModule({
     imports: [
@@ -40,10 +43,10 @@ import { EventListResolver} from './events/events-list-resolver.service'
     ],
     bootstrap: [EventsAppComponent]
 })
-export class AppModule {}
+export class AppModule { }
 
-function checkDirtyState(component: CreateEventComponent){
-    if(component.isDirty)
+function checkDirtyState(component: CreateEventComponent) {
+    if (component.isDirty)
         return window.confirm('You have not saved this event, do you really want to cancel?')
     return true
 }
