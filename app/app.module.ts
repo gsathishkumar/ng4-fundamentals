424a005eae477c7d5d8857d425f7e3e743f96ddc
Sathishkumar <be.sathish@gmail.com>
2017-08-14 16:53:31 +0530

    Lazy load module - UserModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -12,6 +12,7 @@ import { appRoutes } from './routes'
 import { CreateEventComponent } from './events/create-event.component'
 import { Error404Component } from './errors/404.component'
 import { EventRouteActivator } from './events/event-details/event-router-activator.service'
+import { EventListResolver} from './events/events-list-resolver.service'
 
 @NgModule({
     imports: [
@@ -30,10 +31,19 @@ import { EventRouteActivator } from './events/event-details/event-router-activat
     providers: [
         EventService,
         ToastrService,
-        EventRouteActivator
+        EventRouteActivator,
+        {
+            provide: 'canDeactivateCreateEvent',
+            useValue: checkDirtyState
+        },
+        EventListResolver
     ],
     bootstrap: [EventsAppComponent]
 })
-export class AppModule {
+export class AppModule {}
 
+function checkDirtyState(component: CreateEventComponent){
+    if(component.isDirty)
+        return window.confirm('You have not saved this event, do you really want to cancel?')
+    return true
 }
