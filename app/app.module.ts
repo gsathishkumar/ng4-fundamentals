622e92468822b16567336a9994a31e7b43dcf4e3
Sathishkumar <be.sathish@gmail.com>
2017-08-15 00:34:15 +0530

    Pass data from parent to child component

--- app/app.module.ts
+++ app/app.module.ts
@@ -10,7 +10,8 @@ import {
     CreateEventComponent,
     EventRouteActivator,
     EventListResolver,
-    CreateSessionComponent
+    CreateSessionComponent,
+    SessionListComponent
 } from './events/index'
 
 import { EventsAppComponent } from './events-app.component'
@@ -35,7 +36,8 @@ import { AuthService } from './user/auth.service'
         EventDetailsComponent,
         CreateEventComponent,
         Error404Component,
-        CreateSessionComponent
+        CreateSessionComponent,
+        SessionListComponent
     ],
     providers: [
         EventService,
