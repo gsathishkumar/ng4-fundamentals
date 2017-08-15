ac9a886e4f8a9b9070d4dd049e354dabda9d8ccb
Sathishkumar <be.sathish@gmail.com>
2017-08-15 12:32:56 +0530

    Used builtin Pipe and custom pipe

--- app/app.module.ts
+++ app/app.module.ts
@@ -11,7 +11,8 @@ import {
     EventRouteActivator,
     EventListResolver,
     CreateSessionComponent,
-    SessionListComponent
+    SessionListComponent,
+    DurationPipe
 } from './events/index'
 
 import { EventsAppComponent } from './events-app.component'
@@ -39,7 +40,8 @@ import { AuthService } from './user/auth.service'
         Error404Component,
         CreateSessionComponent,
         SessionListComponent,
-        CollapsiblewellComponent
+        CollapsiblewellComponent,
+        DurationPipe
     ],
     providers: [
         EventService,
