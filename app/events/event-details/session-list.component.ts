bff3e03f39c5838717b243973b013cae241ab89a
Sathishkumar <be.sathish@gmail.com>
2017-08-15 13:22:55 +0530

    Added sorting to session list page

--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -4,20 +4,22 @@ import { ISession } from '../shared/index'
     selector: 'session-list',
     templateUrl: '/app/events/event-details/session-list.component.html'
 })
-export class SessionListComponent implements OnChanges{
-    
+export class SessionListComponent implements OnChanges {
+
     @Input() sessions: ISession[]
     @Input() filterBy: string
+    @Input() sortBy: string
     visibleSessions: ISession[] = []
 
     ngOnChanges(): void {
-        if(this.sessions){
+        if (this.sessions) {
             this.filterSessions(this.filterBy)
+            this.sortBy === 'name' ? this.visibleSessions.sort(sortByNameAsc) : this.visibleSessions.sort(sortByVotesDesc)
         }
     }
 
     filterSessions(filterBy): any {
-        if(filterBy === 'all'){
+        if (filterBy === 'all') {
             this.visibleSessions = this.sessions.slice(0)
         } else {
             this.visibleSessions = this.sessions.filter(session => {
@@ -25,4 +27,15 @@ export class SessionListComponent implements OnChanges{
             })
         }
     }
+}
+
+function sortByNameAsc(s1: ISession, s2: ISession): number {
+    if (s1.name > s2.name) return 1
+    else if (s1.name === s2.name) return 0
+    else return -1
+}
+
+
+function sortByVotesDesc(s1: ISession, s2: ISession): number {
+   return s2.voters.length - s1.voters.length
 }
