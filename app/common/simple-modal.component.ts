bdcfe1421c88bf0d2c580d13e936fb3594a5739c
Sathishkumar <be.sathish@gmail.com>
2017-08-15 17:30:11 +0530

    Addeed @ViewChild

--- app/common/simple-modal.component.ts
+++ app/common/simple-modal.component.ts
@@ -1,8 +1,10 @@
-import { Component, Input } from '@angular/core'
+import { Component, Inject, Input, ElementRef, ViewChild } from '@angular/core'
+import { JQ_TOKEN } from './jquery.service'
+
 @Component({
     selector: 'simple-modal',
     template: `
-    <div id="{{elementId}}" class="modal fade" tabindex="-1">
+    <div id="{{elementId}}" #modalcontainer class="modal fade" tabindex="-1">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -11,7 +13,7 @@ import { Component, Input } from '@angular/core'
                     </button>
                     <h4 class="modal-title">{{title}}</h4>
                 </div>
-                <div class="modal-body">
+                <div class="modal-body" (click)="closeModal()">
                     <ng-content></ng-content>
                 </div>
             </div>
@@ -19,10 +21,19 @@ import { Component, Input } from '@angular/core'
     </div>
     `,
     styles: [`
-        .metal-body { height: 250px; overflow-y: scroll;}
+        .modal-body { height: 200px; overflow-y: scroll;}
     `]
 })
 export class SimpleModalComponent {
     @Input() title: string
     @Input() elementId: string
+    @ViewChild('modalcontainer') containerEl: ElementRef
+
+    constructor( @Inject(JQ_TOKEN) private $: any) {
+
+    }
+
+    closeModal() {
+        this.$(this.containerEl.nativeElement).modal('hide')
+    }
 }
